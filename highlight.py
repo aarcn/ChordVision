3de0cb047e002c1f5d7636d765a4589f4fac05aa
aarcn <119821072+aarcn@users.noreply.github.com>
2024-11-14 19:05:27 -0600

    highlighting adapted for two octaves

--- highlight.py
+++ highlight.py
@@ -11,6 +11,18 @@ def clear_highlight(canvas, keys):
     canvas.itemconfig(keys[9], fill="white")
     canvas.itemconfig(keys[10], fill="black")
     canvas.itemconfig(keys[11], fill="white")
+    canvas.itemconfig(keys[12], fill="white")
+    canvas.itemconfig(keys[13], fill="black")
+    canvas.itemconfig(keys[14], fill="white")
+    canvas.itemconfig(keys[15], fill="black")
+    canvas.itemconfig(keys[16], fill="white")
+    canvas.itemconfig(keys[17], fill="white")
+    canvas.itemconfig(keys[18], fill="black")
+    canvas.itemconfig(keys[19], fill="white")
+    canvas.itemconfig(keys[20], fill="black")
+    canvas.itemconfig(keys[21], fill="white")
+    canvas.itemconfig(keys[22], fill="black")
+    canvas.itemconfig(keys[23], fill="white")
 
 
 def highlight_chord(canvas, keys, note_positions, chords, chord_name):
