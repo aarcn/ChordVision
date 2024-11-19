6a10a8dd43b8c6f154361dbc5d1822a18f96d433
aarcn <119821072+aarcn@users.noreply.github.com>
2024-11-19 14:57:45 -0600

    fixed clear_highlight with 88-key config

--- highlight.py
+++ highlight.py
@@ -1,36 +1,17 @@
-def clear_highlight(canvas, keys):
-    canvas.itemconfig(keys[0], fill="white")
-    canvas.itemconfig(keys[1], fill="black")
-    canvas.itemconfig(keys[2], fill="white")
-    canvas.itemconfig(keys[3], fill="black")
-    canvas.itemconfig(keys[4], fill="white")
-    canvas.itemconfig(keys[5], fill="white")
-    canvas.itemconfig(keys[6], fill="black")
-    canvas.itemconfig(keys[7], fill="white")
-    canvas.itemconfig(keys[8], fill="black")
-    canvas.itemconfig(keys[9], fill="white")
-    canvas.itemconfig(keys[10], fill="black")
-    canvas.itemconfig(keys[11], fill="white")
-    canvas.itemconfig(keys[12], fill="white")
-    canvas.itemconfig(keys[13], fill="black")
-    canvas.itemconfig(keys[14], fill="white")
-    canvas.itemconfig(keys[15], fill="black")
-    canvas.itemconfig(keys[16], fill="white")
-    canvas.itemconfig(keys[17], fill="white")
-    canvas.itemconfig(keys[18], fill="black")
-    canvas.itemconfig(keys[19], fill="white")
-    canvas.itemconfig(keys[20], fill="black")
-    canvas.itemconfig(keys[21], fill="white")
-    canvas.itemconfig(keys[22], fill="black")
-    canvas.itemconfig(keys[23], fill="white")
+def clear_highlight(canvas, keys, note_positions):
+    for key_name, pos in note_positions.items():
+        if "#" in key_name:
+            canvas.itemconfig(keys[pos], fill="black")
+        if "b" in key_name:
+            canvas.itemconfig(keys[pos], fill="black")
+        else:
+            canvas.itemconfig(keys[pos], fill="white")
 
 
 def highlight_chord(canvas, keys, note_positions, chords, chord_name):
-    clear_highlight(canvas, keys)
+    clear_highlight(canvas, keys, note_positions)
 
-    notes = chords.get(chord_name)
-    if notes:
-        for note in notes:
-            pos = note_positions.get(note)
-            if pos is not None:
-                canvas.itemconfig(keys[pos], fill="yellow")
+    chord_notes = chords[chord_name]
+    for note in chord_notes:
+        pos = note_positions[note]
+        canvas.itemconfig(keys[pos], fill="yellow")
